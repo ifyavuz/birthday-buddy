94e9052fa8a0cd76cf337cfd2d3911acda79f39c
Fatih Yavuz <ifyavuz@gmail.com>
2024-08-10 05:46:22 +0300

    Geliştirme sonu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
-import './App.css';
-
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
 function App() {
+  const [people, setPeople] = useState(data)
   return (
-    <p>
-      Merhaba Dünya
-    </p>
-  );
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
